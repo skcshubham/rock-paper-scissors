0286c964305afbfea32a437e1a295204b8400ef0
Shubham Kumar <work.shubhamchoudhrary@gmail.com>
2019-10-20 11:36:14 +0530

    Updated UI

--- game.js
+++ game.js
@@ -1,6 +1,69 @@
 let playerScore=0, computerScore=0;
 
-// Generates random integers between 0 and 2
+let rock= document.getElementById("rock");
+let paper= document.getElementById("paper");
+let scissors= document.getElementById("scissors");
+let displayPlayerScore= document.getElementById("playerScore");
+let displayComputerScore= document.getElementById("computerScore");
+let gameStatus= document.getElementById("status");
+let displayPlayerSelection= document.getElementById("playerSelection");
+let displayComputerSelection= document.getElementById("computerSelection");
+let reset= document.getElementById("reset");
+
+
+
+
+play = (playerSelection,computerSelection) => 
+{
+    if(playerSelection === computerSelection)
+    {
+        gameStatus.textContent="It's a draw !!!";
+    }
+    else if (playerSelection === 'scissors') 
+    {
+        if(computerSelection === 'paper') 
+        {
+            playerScore ++;
+            gameStatus.textContent=`Computer loses, ${playerSelection} beats ${computerSelection} !!`;
+        }
+        else
+        {
+            computerScore ++;
+            gameStatus.textContent=`Computer wins, ${computerSelection} beats ${playerSelection} !!`;
+        }
+    }
+    else if (playerSelection === 'rock')
+    {
+        if(computerSelection === 'scissors')
+        {
+            playerScore ++;
+            gameStatus.textContent=`Computer loses, ${playerSelection} beats ${computerSelection} !!`;
+        }
+        else
+        {
+            computerScore ++;
+            gameStatus.textContent=`Computer wins, ${computerSelection} beats ${playerSelection} !!`;
+        }
+    }
+    else if (playerSelection === 'paper')
+    {
+        if(computerSelection === 'rock')
+        {
+            playerScore ++;
+            gameStatus.textContent=`Computer loses, ${playerSelection} beats ${computerSelection} !!`;
+        }
+        else
+        {
+            computerScore ++;
+            gameStatus.textContent=`Computer wins, ${computerSelection} beats ${playerSelection} !!`;
+        }
+    }
+}
+
+
+
+
+
 
 random = () => 
 {
@@ -16,69 +79,54 @@ computerPlay = () =>
     return value[random()];
 }
 
-//playRound() plays one round of Rock Paper Scissor
 
 
-play = (playerSelection,computerSelection) => 
-{
-    if(playerSelection === computerSelection)
-    {
-        return "It's a draw !!!"
-    }
-    else if (playerSelection === 'scissors') 
-    {
-        if(computerSelection === 'paper') 
-        {
-            playerScore ++;
-            return `Computer loses, ${playerSelection} beats ${computerSelection}`;
-        }
-        else
-        {
-            computerScore ++;
-            return `Computer wins, ${computerSelection} beats ${playerSelection}`;
-        }
-    }
-    else if (playerSelection === 'rock')
-    {
-        if(computerSelection === 'scissors')
-        {
-            playerScore ++;
-            return `Computer loses, ${playerSelection} beats ${computerSelection}`;
-        }
-        else
-        {
-            computerScore ++;
-            return `Computer wins, ${computerSelection} beats ${playerSelection}`;
-        }
-    }
-    else if (playerSelection === 'paper')
-    {
-        if(computerSelection === 'rock')
-        {
-            playerScore ++;
-            return `Computer loses, ${playerSelection} beats ${computerSelection}`;
-        }
-        else
-        {
-            computerScore ++;
-            return `Computer wins, ${computerSelection} beats ${playerSelection}`;
-        }
-    }
-}
 
-game = () =>
-{
-    while(playerScore != 5 || computerScore != 5)
-    {
-        if(playerScore === 5 || computerScore === 5)
-            break;
-        let playerSelection = (prompt("Choose between Rock, Paper and Scissors! ")).toLowerCase();
-        let computerSelection = computerPlay();
-        console.log(play(playerSelection,computerSelection));
-    }
-}
 
-console.log(game());
+rock.addEventListener("click",function(){
+    let playerSelection= "rock";
+    displayPlayerSelection.textContent= playerSelection;
+    let computerSelection= computerPlay();
+    displayComputerSelection.textContent= computerSelection;
+    play(playerSelection,computerSelection);
 
-console.log("Player Score",playerScore);
-console.log("Computer Score",computerScore);
+    displayPlayerScore.textContent= playerScore;
+    displayComputerScore.textContent= computerScore;
+    
+});
+
+
+paper.addEventListener("click",function(){
+    let playerSelection= "paper";
+    displayPlayerSelection.textContent= playerSelection;
+    let computerSelection= computerPlay();
+    displayComputerSelection.textContent= computerSelection;
+    play(playerSelection,computerSelection);
+
+    displayPlayerScore.textContent= playerScore;
+    displayComputerScore.textContent= computerScore;
+    
+});
+
+scissors.addEventListener("click",function(){
+    let playerSelection= "scissors";
+    displayPlayerSelection.textContent= playerSelection;
+    let computerSelection= computerPlay();
+    displayComputerSelection.textContent= computerSelection;
+    play(playerSelection,computerSelection);
+
+    displayPlayerScore.textContent= playerScore;
+    displayComputerScore.textContent= computerScore;
+    
+});
+
+reset.addEventListener("click",function(){
+    playerScore=0;
+    computerScore=0;
+    computerSelection.textContent= "Null";
+    playerSelection.textContent= "Null";
+    playerScore.textContent= playerScore;
+    displayComputerScore.textContent= 0;
+    displayPlayerScore.textContent= 0;
+    gameStatus.textContent= "Choose an option !";
+});
